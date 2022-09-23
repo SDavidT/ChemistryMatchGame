7b6460096cc2f087225126e21a881b20136c10c6
Cristian Chancusig <cristianchancusigc@gmail.com>
2022-09-23 11:40:47 -0500

    Sonido

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
+using UnityEngine.UI;
+//using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
     public AudioSource[] destroyNoice;
+    public Slider volControl;
+    public GameObject[] audios;
+
+    private void Start()
+    {
+        audios = GameObject.FindGameObjectsWithTag("audio");
+        volControl.value = PlayerPrefs.GetFloat("volSave", 1f);
+    }
+
+    private void Update()
+    {
+        foreach (GameObject au in audios)
+        {
+            au.GetComponent<AudioSource>().volume = volControl.value;
+        }
+    }
+
+    public void saveVol()
+    {
+        PlayerPrefs.SetFloat("volSave", volControl.value);
+    }
 
     public void PlayRandomDestroyNoice()
     {
@@ -13,11 +35,11 @@ public class SoundManager : MonoBehaviour
         destroyNoice[ClipToPlay].Play();
     }
 
-    [SerializeField] private AudioMixer audioMix;
+    // [SerializeField] private AudioMixer audioMix;
 
-    public void ControlVolume(float volume)
-    {
-        audioMix.SetFloat("Volume", volume);
-    }
+    // public void ControlVolume(float volume)
+    // {
+    //     audioMix.SetFloat("Volume", volume);
+    // }
 
 }
