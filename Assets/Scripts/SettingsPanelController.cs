0d8e26120e4c2726909dbf90b7839810a5579f25
Cristian Chancusig <cristianchancusigc@gmail.com>
2022-09-25 12:54:52 -0500

    menu pause

--- Assets/Scripts/SettingsPanelController.cs
+++ Assets/Scripts/SettingsPanelController.cs
@@ -1,10 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsPanelController : MonoBehaviour
 {
     public Animator panelSettings;
+    public Animator panelHowPlay;
+    public Image soundButton;
+    public Sprite musicOnSprite;
+    public Sprite musicOffSprite;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Sound"))
+        {
+            if (PlayerPrefs.GetInt("Sound") == 0)
+            {
+                soundButton.sprite = musicOffSprite;
+            }
+            else
+            {
+                soundButton.sprite = musicOnSprite;
+            }
+        }
+        else
+        {
+            soundButton.sprite = musicOnSprite;
+        }
+    }
 
     public void ExitSettings()
     {
@@ -21,6 +45,47 @@ public class SettingsPanelController : MonoBehaviour
         {
             panelSettings.SetBool("In", true);
             panelSettings.SetBool("Out2", false);
+        }
+    }
+
+    public void ExitHowPlay()
+    {
+        if (panelHowPlay != null)
+        {
+            panelHowPlay.SetBool("HowOut", true);
+            panelHowPlay.SetBool("HowIn", false);
+        }
+    }
+
+    public void InHowPlay()
+    {
+        if (panelHowPlay != null)
+        {
+            ExitSettings();
+            panelHowPlay.SetBool("HowIn", true);
+            panelHowPlay.SetBool("HowOut", false);
+        }
+    }
+
+    public void SoundButton()
+    {
+        if (PlayerPrefs.HasKey("Sound"))
+        {
+            if (PlayerPrefs.GetInt("Sound") == 0)
+            {
+                soundButton.sprite = musicOnSprite;
+                PlayerPrefs.SetInt("Sound", 1);
+            }
+            else
+            {
+                soundButton.sprite = musicOffSprite;
+                PlayerPrefs.SetInt("Sound", 0);
+            }
+        }
+        else
+        {
+            soundButton.sprite = musicOffSprite;
+            PlayerPrefs.SetInt("Sound", 1);
         }
     }
 }
